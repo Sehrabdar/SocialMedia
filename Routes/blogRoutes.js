b527c14964285a918a4947bbdce1bc06b1019afe
Sehrab Riyaz <darsehraab@gmail.com>
2025-11-18 22:35:56 +0530

    Added delete post method and get all posts method.

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authToken from '../Middleware/auth.js';
 import blogpost from '../Models/Blogpost.js';
+import user from '../Models/Users.js';
 
 const router = express.Router();
 
@@ -13,12 +14,26 @@ router.post('/post', authToken, async (req, res) => {
 
 router.put('/post/:id', authToken, async(req, res) => {
     const post = await blogpost.findByPk(req.params.id);
-    if(!post) return res.status(404).json({message: 'Pos not found!'});
+    if(!post) return res.status(404).json({message: 'Post not found!'});
     if(post.userid !== req.user.id) return res.status(403).json({message: 'Not Authorized!'});
 
     const {title, content} = req.body;
     await post.update({title, content});
     res.json(post);
+});
+
+router.delete('/post/:id', authToken, async(req, res) => {
+    const post = await blogpost.findByPk(req.params.id);
+    if(!post) return res.status(404).json({message: 'Post not found!'});
+    if(post.userid !== req.user.id) return res.status(403).json({message: 'Not Authorized!'});
+
+    await post.destroy();
+    res.json({message: 'Post deleted'});
+});
+
+router.get('', async(req, res) => {
+    const posts = await blogpost.findAll({include: [{model: user, attributes:['username']}]});
+    res.json(posts);
 });
 
 export default router;
