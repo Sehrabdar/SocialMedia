eb7b90d9f62b3f467c705da5ac99591078a8d57e
Sehrab Riyaz <darsehraab@gmail.com>
2025-11-18 22:24:10 +0530

    Added put method so that authorized user can update his/her blogpost title or content.

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -11,5 +11,15 @@ router.post('/post', authToken, async (req, res) => {
     res.status(201).json(post);
 });
 
+router.put('/post/:id', authToken, async(req, res) => {
+    const post = await blogpost.findByPk(req.params.id);
+    if(!post) return res.status(404).json({message: 'Pos not found!'});
+    if(post.userid !== req.user.id) return res.status(403).json({message: 'Not Authorized!'});
+
+    const {title, content} = req.body;
+    await post.update({title, content});
+    res.json(post);
+});
+
 export default router;
 
