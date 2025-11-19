c70e4da9e9efebed0f3cb80e12c8a94bd6652254
Sehrab Riyaz <darsehraab@gmail.com>
2025-11-19 21:17:42 +0530

    Added size limitations and extension restraints to posting pictures.

--- Routes/postPicsRoutes.js
+++ Routes/postPicsRoutes.js
@@ -15,7 +15,20 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage: storage});
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg'){
+        cb(new Error('Only JPEG images are allowed'), false);
+    } else {
+        cb(null, true);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 1*1024*1024},
+    fileFilter: fileFilter
+});
+
 
 const router = express.Router();
 
